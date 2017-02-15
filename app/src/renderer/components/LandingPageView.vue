b07ee5bb197ab34ff836a6b79e559e759a2b4109
Átila Camurça <camurca.home@gmail.com>
2017-02-15 19:41:09 -0300

    Initial commit

---
+++ app/src/renderer/components/LandingPageView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <img src="./LandingPageView/assets/logo.png" alt="remote-pad">
+    <h1>Welcome to Remote Pad GUI</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'landing-page'
+}
+</script>
+
+<style scoped>
+img {
+    margin-top: -25px;
+    width: 250px;
+}
+</style>
