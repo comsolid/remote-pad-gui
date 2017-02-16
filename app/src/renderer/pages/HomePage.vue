ba27ba932b85037bdeb5b99ade7728cfe993d73f
Átila Camurça <camurca.home@gmail.com>
2017-02-16 11:45:00 -0300

    Mockup da Seção Services mostrando a lista de serviços e status

--- app/src/renderer/pages/HomePage.vue
+++ app/src/renderer/pages/HomePage.vue
@@ -1,16 +1,19 @@
 <template lang="html">
     <div>
         <player-section></player-section>
+        <service-section></service-section>
     </div>
 </template>
 
 <script>
 import PlayerSection from 'components/home/PlayerSection'
+import ServiceSection from 'components/home/ServiceSection'
 
 export default {
     name: 'home-page',
     components: {
-        PlayerSection
+        PlayerSection,
+        ServiceSection
     }
 }
 </script>
