9626608bf2b8a7fb17b9fd05c56a63a1fc07770e
Átila Camurça <camurca.home@gmail.com>
2017-03-20 11:39:39 -0300

    Configuration type label for each emulator
    Indicates if the configuration is:
    
    * Plug and Play
    * One Time Configuration
    * Manual
    
    Fixed #38.

---
+++ app/src/renderer/components/emulators/ConfigurationType.vue
@@ -0,0 +1,51 @@
+<template lang="html">
+    <div class="root">
+        <span class="tag" :class="[color]">{{text}}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'configuration-type',
+    props: {
+        type: {
+            type: String,
+            required: false,
+            default: 'plug-and-play'
+        }
+    },
+    data () {
+        return {
+            text: 'Undefined type',
+            color: 'is-danger',
+            types: {
+                'plug-and-play': {
+                    text: 'Plug and Play',
+                    color: 'is-success'
+                },
+                'one-time-configuration': {
+                    text: 'One Time Configuration',
+                    color: 'is-warning'
+                },
+                'manual': {
+                    text: 'Manual',
+                    color: 'is-dark'
+                }
+            }
+        }
+    },
+    mounted () {
+        if (this.types[this.type]) {
+            this.text = this.types[this.type].text
+            this.color = this.types[this.type].color
+        }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+.root {
+    display: flex;
+    margin: 10px auto;
+}
+</style>
